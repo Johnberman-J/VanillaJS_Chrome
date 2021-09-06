724367bc661a8933d1e88607b274880cb5805cc5
Johnberman-J <86252835+Johnberman-J@users.noreply.github.com>
2021-09-06 12:47:08 +0900

    Add files via upload
    first HW

---
+++ calculator.js
@@ -0,0 +1,14 @@
+const calculator = {
+    add : function(a,b) {
+        console.log(a+b);
+    },
+    minus : function(a,b) {
+        console.log(a-b);
+    },
+    div : function(a,b) {
+        console.log(a/b);
+    },
+    powerOf : function(a,b) {
+        console.log(a**b);
+    }
+}
