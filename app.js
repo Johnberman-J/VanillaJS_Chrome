abf78b401eeae12c55436bd662eaa024506ca3c2
Johnberman-J <86252835+Johnberman-J@users.noreply.github.com>
2021-09-09 18:25:31 +0900

    prevent Event; username display;

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const loginForm = document.querySelector(".login-form");
+const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
 
-function buttonClick() {
-    const userName = loginInput.value;
-    console.log(userName);
+const hiddenClassName = "hidden";
+
+// event 정보를 담은 부분을 호출하는 방법.
+function loginSubmit(info) {
+    info.preventDefault()
+    const username = loginInput.value;
+    loginForm.classList.add(hiddenClassName);
+    greeting.innerText = "Hello " + username; // `Hello &{username}`을 
+    greeting.classList.remove(hiddenClassName); // 아웃풋으로 지정해도 똑같이 나옴
 }
 
-loginButton.onclick = buttonClick;
+loginForm.addEventListener("submit",loginSubmit);
