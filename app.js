2c558aaa571c3a24f8d215379ff089847ce0ce0a
Johnberman-J <86252835+Johnberman-J@users.noreply.github.com>
2021-09-10 13:18:47 +0900

    event prevent function;

--- app.js
+++ app.js
@@ -2,15 +2,27 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
 
-const hiddenClassName = "hidden";
+const HIDDENCLASSNAME = "hidden";
+const USERNAMEKEY = "username";
 
 // event 정보를 담은 부분을 호출하는 방법.
 function loginSubmit(info) {
     info.preventDefault()
-    const username = loginInput.value;
-    loginForm.classList.add(hiddenClassName);
-    greeting.innerText = "Hello " + username; // `Hello &{username}`을 
-    greeting.classList.remove(hiddenClassName); // 아웃풋으로 지정해도 똑같이 나옴
+    const iptName = loginInput.value;
+    localStorage.setItem(USERNAMEKEY, iptName);
+    loginForm.classList.add(HIDDENCLASSNAME);
+    pageGreeting(iptName);
 }
 
-loginForm.addEventListener("submit",loginSubmit);
+function pageGreeting(iptName) {
+    greeting.innerText = `Hello ${iptName}`;
+    greeting.classList.remove(HIDDENCLASSNAME);
+}
+const storagedUsername = localStorage.getItem(USERNAMEKEY)
+
+if(storagedUsername===null) {
+    loginForm.classList.remove(HIDDENCLASSNAME);
+    loginForm.addEventListener("submit",loginSubmit);
+} else {
+    pageGreeting(storagedUsername);
+}
