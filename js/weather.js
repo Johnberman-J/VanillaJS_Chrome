31dc5c154cfd10a000395a027317b358a0f899cf
Johnberman-J <86252835+Johnberman-J@users.noreply.github.com>
2021-09-16 19:51:38 +0900

    Add files via upload

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,15 @@
+const API_KEY = "5b78057bba84753a53e5f3aa6fb67519";
+
 function onGeoSuccess(location) {
-    console.log(location);
+    const latitude = "37.3501761";
+    const longitude = "127.11512619999999";
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const weatherSelector = document.querySelector("#weather span:first-child");
+        const citySelector = document.querySelector("#weather span:last-child");
+        citySelector.innerText = data.name;
+        weatherSelector.innerText = data.weather[0].main;
+    });
 }
 
 function onGeoError() {
